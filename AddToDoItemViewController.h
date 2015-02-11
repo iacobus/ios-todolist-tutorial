15fdfdcf13bd568af0c966b3af7762caa99218c2
Jacobo Blasco <jacobo@casecommons.org>
2015-02-10 21:01:48 -0500

    Finish Storyboards tutorial.

--- AddToDoItemViewController.h
+++ AddToDoItemViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AddToDoItemViewController.h
 //  ToDoList
 //
 //  Created by Jacobo on 2/10/15.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AddToDoItemViewController : UIViewController
 
 @end
---
+++ AddToDoItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddToDoItemViewController.m
+//  ToDoList
+//
+//  Created by Jacobo on 2/10/15.
+//  Copyright (c) 2015 Jacobo. All rights reserved.
+//
+
+#import "AddToDoItemViewController.h"
+
+@interface AddToDoItemViewController ()
+
+@end
+
+@implementation AddToDoItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
